43b59f64ed93b57dea491c0f6fb74a5af45296f6
Thomas 'sitzz' Vang <8313369+sitzz@users.noreply.github.com>
2024-12-07 16:08:17 +0100

    feat: added params nameserver_type and nameservers
    Added params to the `cloudns_dns_zone` resource:
    - Added argument `nameserver_type` which can be any of "all", "free", or
      "premium" which will assign all, free or premium ClouDNS nameservers
      respectively to the zone upon creation.
    - Added argument `nameservers` which is a list of nameservers which
      should be assigned to the zone upon creation.
    
    The params are conflicts with each other, so only one may be set.

--- internal/cloudns/resource_dns_zone.go
+++ internal/cloudns/resource_dns_zone.go
@@ -3,6 +3,8 @@ package cloudns
 import (
 	"context"
 	"fmt"
+	"slices"
+	"sort"
 	"strings"
 
 	"github.com/ClouDNS/cloudns-go"
@@ -31,17 +33,42 @@ func resourceDnsZone() *schema.Resource {
 				Required:    true,
 				ForceNew:    true,
 			},
-			"type": {
-				Description: "The type of the DNS zone (master/slave/parked/geodns).",
-				Type:        schema.TypeString,
-				Required:    true,
-				ForceNew:    true,
-			},
 			"master": {
 				Description: "Master IP for slave zone",
 				Type:        schema.TypeString,
 				Optional:    true,
 				ForceNew:    false,
+			},
+			"nameserver_type": {
+				Description:   "The type of nameservers to use (all/free/premium.)",
+				Type:          schema.TypeString,
+				Optional:      true,
+				ForceNew:      true,
+				ConflictsWith: []string{"nameservers"},
+				ValidateFunc: func(val any, key string) (warns []string, errs []error) {
+					acceptableValues := []string{"all", "free", "premium"}
+					if !slices.Contains(acceptableValues, val.(string)) {
+						errs = append(errs, fmt.Errorf("%q must be one of %s, got %s", key, strings.Join(acceptableValues, ", "), val))
+					}
+					return
+				},
+			},
+			"nameservers": {
+				Description:   "A set of NS servers to use for this zone.",
+				Type:          schema.TypeList,
+				Optional:      true,
+				ForceNew:      true,
+				ConflictsWith: []string{"nameserver_type"},
+				Computed:      true,
+				Elem: &schema.Schema{
+					Type: schema.TypeString,
+				},
+			},
+			"type": {
+				Description: "The type of the DNS zone (master/slave/parked/geodns).",
+				Type:        schema.TypeString,
+				Required:    true,
+				ForceNew:    true,
 			},
 		},
 	}
@@ -51,12 +78,26 @@ func resourceDnsZoneCreate(ctx context.Context, d *schema.ResourceData, meta int
 	clientConfig := meta.(ClientConfig)
 	zoneToCreate := toApiZone(d)
 
+	// Determine (and eventually sort) NS records
+	var ns []string
+	_, nsExist := d.GetOk("nameservers")
+	_, nstExist := d.GetOk("nameserver_type")
+	if nsExist || nstExist {
+		nsList, _ := cloudns.Ns{}.List(clientConfig.apiAccess)
+		zoneToCreate.Ns = getNsNames(d, nsList)
+		d.Set("nameservers", zoneToCreate.Ns)
+	}
+
 	resp, err := zoneToCreate.Create(&clientConfig.apiAccess)
 	if err != nil {
 		return diag.FromErr(err)
 	}
 
-	tflog.Debug(ctx, fmt.Sprintf("CREATE DNS zone: %s, type: %s", resp.Domain, resp.Ztype))
+	if len(ns) == 0 {
+		tflog.Debug(ctx, fmt.Sprintf("CREATE DNS zone: %s, type: %s", resp.Domain, resp.Ztype))
+	} else {
+		tflog.Debug(ctx, fmt.Sprintf("CREATE DNS zone: %s, type: %s, ns: %s", resp.Domain, resp.Ztype, strings.Join(ns, ", ")))
+	}
 
 	d.SetId(zoneToCreate.Domain)
 	return resourceDnsZoneRead(ctx, d, meta)
@@ -64,13 +105,13 @@ func resourceDnsZoneCreate(ctx context.Context, d *schema.ResourceData, meta int
 
 func resourceDnsZoneRead(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
 	clientConfig := meta.(ClientConfig)
-	lookup := toApiZone(d)
+	zoneToRead := toApiZone(d)
 
 	clientConfig.rateLimiter.Take()
-	zoneRead, err := lookup.Read(&clientConfig.apiAccess)
+	zoneRead, err := zoneToRead.Read(&clientConfig.apiAccess)
 	if err != nil {
 		if isNotFoundErr(err) {
-			tflog.Warn(ctx, fmt.Sprintf("DNS zone not found: %s. Removing from state.", lookup.Domain))
+			tflog.Warn(ctx, fmt.Sprintf("DNS zone not found: %s. Removing from state.", zoneToRead.Domain))
 			d.SetId("")
 			return nil
 		}
@@ -78,13 +119,20 @@ func resourceDnsZoneRead(ctx context.Context, d *schema.ResourceData, meta inter
 	}
 
 	if zoneRead.Domain == "" {
-		tflog.Warn(ctx, fmt.Sprintf("Received unexpected empty response for DNS zone: %s. Removing from state.", lookup.Domain))
+		tflog.Warn(ctx, fmt.Sprintf("Received unexpected empty response for DNS zone: %s. Removing from state.", zoneToRead.Domain))
 		d.SetId("")
 		return nil
 	}
 
+	nsRecords, err := getFilteredZoneRecords(zoneToRead, clientConfig, []string{"NS"})
+	if err == nil && len(nsRecords) > 0 {
+		zoneRead.Ns = sortNsNames(nsRecords)
+	}
+
 	d.Set("domain", zoneRead.Domain)
 	d.Set("type", zoneRead.Ztype)
+	d.Set("nameservers", zoneRead.Ns)
+
 	return nil
 }
 
@@ -108,17 +156,23 @@ func resourceDnsZoneDelete(ctx context.Context, d *schema.ResourceData, meta int
 }
 
 func resourceDnsZoneImport(ctx context.Context, d *schema.ResourceData, meta interface{}) ([]*schema.ResourceData, error) {
-	config := meta.(ClientConfig)
+	clientConfig := meta.(ClientConfig)
 	domain := d.Id()
 
-	config.rateLimiter.Take()
-	zoneRead, err := cloudns.Zone{Domain: domain}.Read(&config.apiAccess)
+	clientConfig.rateLimiter.Take()
+	zoneToRead := cloudns.Zone{Domain: domain}
+	zoneRead, err := zoneToRead.Read(&clientConfig.apiAccess)
 	if err != nil {
 		return nil, err
 	}
 
 	if zoneRead.Domain == "" {
 		return nil, fmt.Errorf("Zone not found: %#v", domain)
+	}
+
+	nsRecords, err := getFilteredZoneRecords(zoneToRead, clientConfig, []string{"NS"})
+	if err == nil && len(nsRecords) > 0 {
+		zoneRead.Ns = sortNsNames(nsRecords)
 	}
 
 	err = updateZoneState(d, &zoneRead)
@@ -147,7 +201,68 @@ func updateZoneState(d *schema.ResourceData, zone *cloudns.Zone) error {
 		}
 	}
 
+	if err := d.Set("nameservers", zone.Ns); err != nil {
+		return err
+	}
+
 	return nil
+}
+
+func sortNsNames(uns []string) []string {
+	sort.Slice(uns, func(i, j int) bool {
+		return uns[i] < uns[j]
+	})
+
+	var rns []string
+	rns = append(rns, uns...)
+
+	return rns
+}
+
+func getNsNames(d *schema.ResourceData, nsList []cloudns.Ns) []string {
+	ns, isset := d.GetOk("nameservers")
+	if isset {
+		var rns []string
+		for _, rec := range ns.([]interface{}) {
+			rns = append(rns, rec.(string))
+		}
+		return slices.CompactFunc(rns, strings.EqualFold)
+	}
+
+	filter, isset := d.GetOk("nameserver_type")
+	if !isset || filter == "all" {
+		return nil
+	}
+
+	var fns []string
+	for _, ns := range nsList {
+		if filter == ns.Type {
+			fns = append(fns, ns.Name)
+		}
+	}
+
+	// we compact and sort NS records to avoid creating diffs should there be changes in the API response
+	fns = slices.CompactFunc(fns, strings.EqualFold)
+	fns = sortNsNames(fns)
+
+	return fns
+}
+
+func getFilteredZoneRecords(z cloudns.Zone, c ClientConfig, filter []string) ([]string, error) {
+	// TODO: functionality should be moved to the `cloudns-go` repository
+	zoneRecords, err := z.List(&c.apiAccess)
+	if err != nil && len(zoneRecords) == 0 {
+		return nil, fmt.Errorf("found no records zone for %s", z.Domain)
+	}
+
+	var rns []string
+	for _, rec := range zoneRecords {
+		if len(filter) == 0 || slices.Contains(filter, rec.Rtype) {
+			rns = append(rns, rec.Record)
+		}
+	}
+
+	return rns, nil
 }
 
 func toApiZone(d *schema.ResourceData) cloudns.Zone {
@@ -159,6 +274,7 @@ func toApiZone(d *schema.ResourceData) cloudns.Zone {
 		Domain: domain,
 		Ztype:  zoneType,
 		Master: master,
+		Ns:     []string{},
 	}
 }
 
